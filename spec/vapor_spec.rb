86619796cce6f1b403a70e5b5a98435c619b8d42
Balint Erdi <balint.erdi@gmail.com>
2010-02-11 10:04:02 +0100

    generates clouds from yaml file (and adds cloudifiability to Hash)

---
+++ spec/vapor_spec.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), '..', 'vapor.rb')
+require 'bacon'
+require 'pathname'
+require 'yaml'
+
+describe Vapor do
+
+  before do
+    class Hash; include Cloudifiable; end
+  end
+
+  describe Cloudifiable do
+    should "return correct rendered html cloud" do
+      languages = { "Hungarian" => 10, "English" => 9, "French" => 7, "Spanish" => 8, "Catalan" => 4 }
+      languages.cloudify.should.be.equal(%q(<span class="cw_10">Hungarian</span> <span class="cw_9">English</span> <span class="cw_8">Spanish</span> <span class="cw_7">French</span> <span class="cw_4">Catalan</span>))
+    end
+  end
+
+  it "can generate clouds from yaml files" do
+    cwd = Pathname.new(File.dirname(__FILE__))
+    cloudified = Vapor.cloudify(cwd.join('../things.yml'))
+    cloudified.should.include(%q(<span class="cw_10">books</span> <span class="cw_7">hiking</span> <span class="cw_7">cycling</span> <span class="cw_6">economics</span> <span class="cw_5">music</span>))
+    cloudified.should.include(%q(<span class="cw_10">ruby</span> <span class="cw_6">javascript</span> <span class="cw_5">clojure</span> <span class="cw_3">java</span> <span class="cw_2">php</span>))
+  end
+end
